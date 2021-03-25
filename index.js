ce335fc1fb0f4b2e22b367cffebb7f2a18f209f8
LHY-iS-Learning <hongyi.lyu95@gmail.com>
2021-03-24 22:07:53 -0700

    add relevent files

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  extends: [
+    'airbnb-typescript',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+  ],
+}
